195b9cdfc38c14410dee07a110c2ec4b14b681d6
Evocel <lorenzo.g.rosellini@gmail.com>
2018-04-17 14:18:00 -0500

    Add files via upload

---
+++ Test_sketch.ino
@@ -0,0 +1,143 @@
+#include "Constants.h"
+
+void loop() {
+//This program is designed to play Row Row Row your boat
+//CCCDE "Row, row, row your boat"
+  tone(PORT,C4);
+  delay(400);
+  noTone(PORT);
+  delay(100);
+
+  tone(PORT,C4);
+  delay(400);
+  noTone(PORT);
+  delay(100);
+
+  tone(PORT,C4);
+  delay(300);
+  noTone(PORT);
+  delay(60);
+
+  tone(PORT,D4);
+  delay(200);
+  noTone(PORT);
+
+  tone(PORT,E4);
+  delay(200);
+  noTone(PORT);
+  delay(300);
+//EDEFG "Gently down the stream"
+  tone(PORT,E4);
+  delay(200);
+  noTone(PORT);
+  delay(60);
+
+  tone(PORT,D4);
+  delay(200);
+  noTone(PORT);
+  delay(20);
+
+  tone(PORT,E4);
+  delay(200);
+  noTone(PORT);
+  delay(60);
+
+  tone(PORT,F4);
+  delay(200);
+  noTone(PORT);
+  delay(20);
+
+  tone(PORT,G4);
+  delay(200);
+  noTone(PORT);
+  delay(600);
+//CCCGGGEEECCC "Merrily merrily merrily merrily"
+  tone(PORT,C5);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+
+  tone(PORT,C5);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+
+  tone(PORT,C5);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+
+
+  tone(PORT,G4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+
+  tone(PORT,G4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+
+  tone(PORT,G4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+  
+
+  tone(PORT,E4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+
+  tone(PORT,E4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+  
+  tone(PORT,E4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+  
+   
+  tone(PORT,C4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+
+  tone(PORT,C4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+
+  tone(PORT,C4);
+  delay(200);
+  noTone(PORT);
+  delay(10);
+  
+//GFEDC "Life is but a dream"
+  tone(PORT,G4);
+  delay(200);
+  noTone(PORT);
+  delay(60);
+
+  tone(PORT,F4);
+  delay(200);
+  noTone(PORT);
+  delay(20);
+
+  tone(PORT,E4);
+  delay(200);
+  noTone(PORT);
+  delay(60);
+
+  tone(PORT,D4);
+  delay(200);
+  noTone(PORT);
+  delay(60);
+
+  tone(PORT,C4);
+  delay(200);
+  noTone(PORT);
+  delay(1000);
+}
